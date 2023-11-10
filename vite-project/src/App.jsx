64a7024f274cd22d2ff13b217d8c35c4fae4cb24
SanaUllahRasheed123 <starsreachersacademy@gmail.com>
2023-11-10 23:04:18 +0500

    updated

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="bg-#fffbeb">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
